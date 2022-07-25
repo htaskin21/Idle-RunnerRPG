25d47c46daa6db766a0f844c83ce651b0ac185bc
hasan-veloxia <hasan@veloxia.co>
2022-07-25 23:26:47 +0300

    fix(background-controller): fix parallax effect of sky images

--- Assets/Scripts/SkyImage.cs
+++ Assets/Scripts/SkyImage.cs
@@ -1,10 +1,18 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class SkyImage : MonoBehaviour
 {
     public SpriteRenderer firstLayer;
-    
+
     public SpriteRenderer secondLayer;
-    
+
     public SpriteRenderer thirdLayer;
+
+    private void Start()
+    {
+        var go = gameObject;
+        go.transform.DOMoveX(go.transform.position.x + 1, 1.05f).SetLoops(-1, LoopType.Incremental)
+            .SetEase(Ease.Linear);
+    }
 }
