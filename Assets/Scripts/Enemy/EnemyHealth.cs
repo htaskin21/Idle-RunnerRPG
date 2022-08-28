b4eae4dc0efa73d34d7b5bb46985ce8498e8003a
hasan-veloxia <hasan@veloxia.co>
2022-08-28 21:01:30 +0300

    refactor(enemy): add fade out effect on health bar

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -9,14 +9,19 @@ namespace Enemy
     {
         [SerializeField]
         private float maxHealth;
+
         public float MaxHealth => maxHealth;
 
         [SerializeField]
         private float health;
+
         public float Health => health;
 
         [SerializeField]
         private Slider healthBar;
+
+        [SerializeField]
+        private CanvasGroup healthBarCanvasGroup;
 
         public Action OnEnemyDie;
 
@@ -32,12 +37,19 @@ namespace Enemy
             healthBar.minValue = 0;
             healthBar.maxValue = maxHealth;
             healthBar.value = health;
+
+            healthBarCanvasGroup.alpha = 1;
         }
 
         public void SetHealth(float attackDamage)
         {
             health -= attackDamage;
             healthBar.DOValue(health, 0.2f);
+
+            if (health <= 0)
+            {
+                healthBarCanvasGroup.DOFade(0, .35f).SetEase(Ease.InCirc);
+            }
         }
     }
 }
