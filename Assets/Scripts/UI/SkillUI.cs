a1d0affbba61daa659ec04529fc4784c8cc3a403
hasan-veloxia <hasan@veloxia.co>
2022-09-25 13:34:35 +0300

    refactor(ui): create skill panel

---
+++ Assets/Scripts/UI/SkillUI.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace UI
+{
+    public class SkillUI : MonoBehaviour
+    {
+        private int _id;
+
+        private Sprite _icon;
+
+        private SkillTypes _skillTypes;
+
+        private double _incrementAmount;
+    }
+}
